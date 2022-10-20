d26329be4c9b3a4aa0bb4eaf1241886e295a83cb
נהוראי <nehoraimimon@gmail.com>
2022-10-20 15:40:14 +0300

    user

--- Desktop/HomeWork/Client side/my-app/src/App.js
+++ Desktop/HomeWork/Client side/my-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Components/Header';
 import Upload from './Components/Upload';
+import User from './Components/User';
 
 function App() {
   return (<>
     <Header/>
+    <div className='upload-user'>
     <Upload/>
+    <User/>
+    </div>
   </>
   );
 }
