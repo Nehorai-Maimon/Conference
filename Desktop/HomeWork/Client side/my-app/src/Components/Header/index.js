5ce54dc6ac983da335d509029697840370e0b307
נהוראי <nehoraimimon@gmail.com>
2022-10-20 12:07:05 +0300

    header

---
+++ Desktop/HomeWork/Client side/my-app/src/Components/Header/index.js
@@ -0,0 +1,10 @@
+import styles from "./style.module.css"
+// import {AiOutlineCloudUpload} from "react-icons/ai"
+
+export default function Header(){
+    return <div className={styles.container}>
+     <div className={styles.head} >Drive</div>
+     {/* <AiOutlineCloudUpload/>
+     <div>nehorai</div> */}
+    </div>
+}
