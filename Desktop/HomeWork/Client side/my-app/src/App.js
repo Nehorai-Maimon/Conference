f19201782d2f6d1b347f428837526329ceecefa2
נהוראי <nehoraimimon@gmail.com>
2022-10-20 14:33:18 +0300

    upload

--- Desktop/HomeWork/Client side/my-app/src/App.js
+++ Desktop/HomeWork/Client side/my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Upload from './Components/Upload';
 
 function App() {
-  return (
+  return (<>
     <Header/>
-    
+    <Upload/>
+  </>
   );
 }
 
--- Desktop/HomeWork/Client side/my-app/src/Components/Header/index.js
+++ Desktop/HomeWork/Client side/my-app/src/Components/Header/index.js
@@ -2,9 +2,9 @@ import styles from "./style.module.css"
 // import {AiOutlineCloudUpload} from "react-icons/ai"
 
 export default function Header(){
-    return <div className={styles.container}>
-     <div className={styles.head} >Drive</div>
+    return <>
+     <div className={styles.top} >Drive</div>
      {/* <AiOutlineCloudUpload/>
      <div>nehorai</div> */}
-    </div>
+    </>
 }
